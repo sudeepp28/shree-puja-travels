bb673c50b57c96fde667fca1a695c24383ab6be1
sudeepp28 <122432690+sudeepp28@users.noreply.github.com>
2025-07-16 17:59:48 +0530

    dfd

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-email='email'
+email='shreepujatraveldesk@gmail.com'
 fblink='https://www.facebook.com/people/My-Journey-Cabs/100067590563390/?rdid=gjWdl9JDmIbmDGB2&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2F16TEeq5ffh%2F'
 
 }
